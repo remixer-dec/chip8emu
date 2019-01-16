fc18d1354ce0e1173ce4bd25db1f7dc99c328bd7
remixer-dec <remixer.dec@gmail.com>
2019-01-17 01:14:11 +0300

    v0.1.5
    -tabs for mobile UI
    -small css improvements

--- main.js
+++ main.js
@@ -5,3 +5,45 @@ romselector.addEventListener('change', romLoader.loader)
 romLoader.setOnloadCbk(()=>{
     emulator.emulate(romLoader.MEMORY)
 });
+
+//UI
+window.onload = ()=>{
+    setTimeout(()=>{
+        /*web components imports experiment*/
+        var component = document.querySelector('link[rel="import"]').import;
+        var table = component.querySelector('#opcodes');
+        //this has to be done to fix Firefox ID duplication bug
+        var ins = table.cloneNode(true)
+        table.remove()
+        document.body.appendChild(ins)
+        if(document.body.clientWidth<600){
+            mobiletabs.firstElementChild.click();
+        }
+    },500);
+    function show(arr){for(let i of arr){i.classList.remove('hidden')}}
+    function hide(arr){for(let i of arr){i.classList.add('hidden')}}
+    function tabs(e){
+        e = parseInt(e.target.getAttribute('name'))
+        switch(e){
+            case 0:
+                show([controls,dbgc])
+                hide([config,dbgtools,changelog,opcodes])
+            break
+            case 1:
+                show([config])
+                hide([controls,dbgc,dbgtools,changelog,opcodes])
+            break
+            case 2:
+                show([dbgc,dbgtools])
+                hide([controls,config,changelog,opcodes])
+            break
+            case 3:
+                show([opcodes,changelog])
+                hide([config,controls,dbgc,dbgtools])
+            break
+        }
+    }
+    for(let tab of mobiletabs.children){
+        tab.addEventListener('click',tabs)
+    }
+}
